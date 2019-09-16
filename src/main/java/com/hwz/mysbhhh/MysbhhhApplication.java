ff0b26b4b22d83b4339e389b2393d3efb9a26bd8
hewenzhong <420439430@qq.com>
2019-09-16 15:43:49 +0800

    springboot测试项目

---
+++ src/main/java/com/hwz/mysbhhh/MysbhhhApplication.java
@@ -0,0 +1,13 @@
+package com.hwz.mysbhhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysbhhhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysbhhhApplication.class, args);
+    }
+
+}
